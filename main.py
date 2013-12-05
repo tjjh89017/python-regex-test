6b82148502601e4865ee5319c7838e9d4a2266bb
Date <tjjh89017@hotmail.com>
2013-12-05 19:52:03 +0800

    fix

--- main.py
+++ main.py
@@ -4,20 +4,28 @@
 import os
 import sys
 import re
+import urllib2
 
 pattern = re.compile('"javascript:if\(confirm\(%27.*%27\)\)window\.location=%27(.*table_detail.*)%27"')
 
+def download_file(url, path = "./"):
+
+	with open(path + url.split('/')[-1], "w") as f:
+		f.write(urllib2.urlopen(url).read())
 
 if __name__ == "__main__":
 
-	path = "."
+	path = "./"
 	if len(sys.argv) > 1:
 		path = sys.argv[1]
 	
 	for abs_path, dir_name, filenames in os.walk(path):
+		print abs_path
 		for filename in filenames:
-			with open(abs_path + filename, "r") as f:
+			with open(abs_path + "/" + filename, "r") as f:
 				html = f.read()
 				result = pattern.findall(html)
-				print result
-
+				for url in result:
+					print "Downloading", url, "...",
+					download_file(url, abs_path)
+					print "done"
